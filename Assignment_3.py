902011d6ba4b7a9ccca4f203adb4835222a1a3f3
Cole Thompson <colet0227@gmail.com>
2023-05-12 20:48:11 -0700

    separated into dif functions for inverted indexer

--- Assignment_3.py
+++ Assignment_3.py
@@ -1,19 +1,43 @@
-# Assignment 3
-
-from bs4 import BeautifulSoup
 import os
+import json
+from collections import defaultdict, Counter
+import re
 
-html_dir = ""
+def parse_json_files(directory):
+    docs = defaultdict(str)
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith('.json'):
+                with open(os.path.join(dirpath, filename), 'r') as f:
+                    data = json.load(f)
+                    docs[filename] = data['content']
+    return docs
 
-text_dict = {}
+def tokenize(docs):
+    pattern = r'[A-Za-z0-9]+'
+    for doc_id, text in docs.items():
+        words = re.findall(pattern, text)
+        yield doc_id, Counter(words)
 
-for file_name in os.listdir(html_dir):
-    if file_name.endswith(".html"):
-        file_path = os.path.join(html_dir, file_name)
-        with open(file_path, "r", encoding="utf-8") as f:
-            # parse HTML with Beautiful Soup
-            soup = BeautifulSoup(f, "html.parser")
-            # extract text content from HTML
-            text = soup.get_text()
-            # store text content in dictionary
-            text_dict[file_name] = text
+def build_index(tokens):
+    index = defaultdict(list)
+    for doc_id, token_counter in tokens:
+        for token, frequency in token_counter.items():
+            index[token].append((doc_id, frequency))
+    return index
+
+def main():
+    docs = parse_json_files('/Users/colethompson/Documents/A3/ANALYST')
+    tokens = tokenize(docs)
+    index = build_index(tokens)
+
+    with open('index.json', 'w') as f:
+        json.dump(index, f)
+    print(f'Size of index: {os.path.getsize("index.json") / 1024} KB')
+
+    print(f'Number of documents: {len(docs)}')
+    print(f'Number of unique tokens: {len(index)}')
+    print(f'Size of index: {os.path.getsize("index.json") / 1024} KB')
+
+if __name__ == '__main__':
+    main()
