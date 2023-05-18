13ebbce4b3e479fbe7d56f384acc7a92043e0aad
Cole Thompson <colet0227@gmail.com>
2023-05-17 18:01:58 -0700

    added retrieval

--- Assignment_3 copy.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-import nltk
-from collections import defaultdict, Counter
-from nltk.corpus import stopwords
-
-# Download the NLTK stop words
-nltk.download('punkt')
-nltk.download('stopwords')
