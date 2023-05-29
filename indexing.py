62ed06bfc4f7ed618fd56cec00ced8a9cf06c75f
Cole Thompson <colet0227@gmail.com>
2023-05-29 14:33:57 -0700

    updated. still need retrieval

--- indexing.py
+++ indexing.py
@@ -1,135 +1,116 @@
-import shutil
-import math
-import csv
-import os
 import json
-
-# This function merges multiple partial indices into a single index
-def merge_partial_indices(partial_index_paths, merged_index_path):
-    # Append the first partial index to the merged index
-    with open(partial_index_paths[0], 'r') as f_in, open(merged_index_path, 'a') as f_out:
-        # Skip header of first partial index
-        next(f_in)
-
-        # Copy the content of the first partial index into the merged index
-        shutil.copyfileobj(f_in, f_out)
-
-    # Delete the first partial index after copying
-    os.remove(partial_index_paths[0])
-
-    # For each remaining partial index, merge it with the current merged index
-    for path in partial_index_paths[1:]:
-        # Temporary merged index path
-        temp_merged_index_path = merged_index_path + '.tmp'
-
-        # Open partial as read only and use the created temporary path to write to
-        with open(path, 'r') as partial_index_file, open(merged_index_path, 'r') as merged_index_file, open(temp_merged_index_path, 'w') as temp_merged_index_file:
-            partial_index_reader = csv.DictReader(partial_index_file)
-            merged_index_reader = csv.DictReader(merged_index_file)
-
-            # Get field names from merged index file
-            fieldnames = merged_index_reader.fieldnames
-
-            # Create a dict writer to write to temporary csv file
-            temp_merged_index_writer = csv.DictWriter(temp_merged_index_file, fieldnames=fieldnames)
-            temp_merged_index_writer.writeheader()
-
-            # Iterator until end and then return none
-            # Represents rows in the partial and merged index csv files
-            partial_row = next(partial_index_reader, None)
-            merged_row = next(merged_index_reader, None)
-
-            # Merge the current rows from the two indices
-            while partial_row and merged_row:
-                if partial_row['Token'] == merged_row['Token']:
-                    merged_data = json.loads(merged_row['Data'])
-                    token_data = json.loads(partial_row['Data'])
-
-                    # Merge the data
-                    merged_data['token_freq'] += token_data['token_freq']
-                    merged_data['document_freq'] += token_data['document_freq']
-                    merged_data['doc_ids'].update(token_data['doc_ids'])
-
-                    # Write a new row to the temporary file
-                    temp_merged_index_writer.writerow({'Token': merged_row['Token'], 'Data': json.dumps(merged_data)})
-                    partial_row = next(partial_index_reader, None)
-                    merged_row = next(merged_index_reader, None)
-
-                elif partial_row['Token'] < merged_row['Token']:
-                    temp_merged_index_writer.writerow(partial_row)
-                    partial_row = next(partial_index_reader, None)
-                else:
-                    temp_merged_index_writer.writerow(merged_row)
-                    merged_row = next(merged_index_reader, None)
-
-            # Process the remaining rows of the longer file
-            while partial_row:
-                temp_merged_index_writer.writerow(partial_row)
-                partial_row = next(partial_index_reader, None)
-            
-            while merged_row:
-                temp_merged_index_writer.writerow(merged_row)
-                merged_row = next(merged_index_reader, None)
-
-        # Delete the old merged index and rename the temp to merged
-        os.remove(merged_index_path)
-        os.rename(temp_merged_index_path, merged_index_path)
-
-        # Delete the processed partial index
-        os.remove(path)
-
-
-# This function generates a secondary index which maps each unique first letter of tokens to the file position where tokens with that first letter start
-# Does this after merged index is already creaetd
-def generate_secondary_index(merged_index_path, secondary_index_path):
-    current_letter = ''
-
-    # Open merged as read only and write to secondary index path
-    with open(merged_index_path, 'r') as index_file, open(secondary_index_path, 'w', newline='') as secondary_index_file:
-        secondary_index_writer = csv.writer(secondary_index_file)
-
-        while True:
-            # .tell() returns position to current cursor
-            current_pos = index_file.tell()
-            line = index_file.readline()
-
-            # Break the loop if we've reached the end of the file
-            if not line:
-                break
-
-            token = line.split(',')[0]  # Assuming token is the first column
-
-            if token[0] != current_letter:
-                current_letter = token[0]
-                secondary_index_writer.writerow([current_letter, current_pos])
-
-    return secondary_index_path
+import math
+import os
+import shutil
 
 def count_documents(url_id_map_path):
     with open(url_id_map_path, 'r') as file:
-        return sum(1 for row in csv.reader(file)) - 1  # Subtract 1 to account for header row
+        return sum(1 for _ in file)  # Count number of lines
+
+def merge_partial_indices(partial_index_paths, merged_index_path):
+    with open(partial_index_paths[0], 'r') as f_in, open(merged_index_path, 'w') as f_out:
+        # Copy the content of the first partial index into the merged index
+        shutil.copyfileobj(f_in, f_out)
+    os.remove(partial_index_paths[0])  # Delete the first partial index after copying
+
+    # For each remaining partial index, merge it with the current merged index
+    for path in partial_index_paths[1:]:
+        temp_merged_index_path = merged_index_path + '.tmp'  # Temporary merged index path
+
+        with open(path, 'r') as partial_index_file, open(merged_index_path, 'r') as merged_index_file, open(temp_merged_index_path, 'w') as temp_merged_index_file:
+            partial_line = next(partial_index_file, None)
+            merged_line = next(merged_index_file, None)
+
+            while partial_line and merged_line:
+                # Skip lines in partial index that don't contain a ":"
+                if ":" not in partial_line:
+                    partial_line = next(partial_index_file, None)
+                    continue
+
+                # Skip lines in merged index that don't contain a ":"
+                if ":" not in merged_line:
+                    merged_line = next(merged_index_file, None)
+                    continue
+
+                partial_token, partial_data = partial_line.strip().split(":", 1)  # Add 1 to split only once
+                merged_token, merged_data = merged_line.strip().split(":", 1)  # Add 1 to split only once
+
+                partial_data = json.loads(partial_data)
+                merged_data = json.loads(merged_data)
+
+                if partial_token == merged_token:
+                    merged_data['token_freq'] += partial_data['token_freq']
+                    merged_data['document_freq'] += partial_data['document_freq']
+                    merged_data['doc_ids'].update(partial_data['doc_ids'])
+
+                    temp_merged_index_file.write(f'{merged_token}: {json.dumps(merged_data)}\n')
+
+                    partial_line = next(partial_index_file, None)
+                    merged_line = next(merged_index_file, None)
+                elif partial_token < merged_token:
+                    temp_merged_index_file.write(partial_line)
+                    partial_line = next(partial_index_file, None)
+                else:
+                    temp_merged_index_file.write(merged_line)
+                    merged_line = next(merged_index_file, None)
+
+            # Process the remaining lines of the longer file
+            while partial_line:
+                temp_merged_index_file.write(partial_line)
+                partial_line = next(partial_index_file, None)
+
+            while merged_line:
+                temp_merged_index_file.write(merged_line)
+                merged_line = next(merged_index_file, None)
+
+        os.remove(merged_index_path)  # Delete the old merged index
+        os.rename(temp_merged_index_path, merged_index_path)  # Rename the temp to merged
+
+        os.remove(path)  # Delete the processed partial index
+
+
+
+def generate_secondary_index(merged_index_path, secondary_index_path):
+    with open(merged_index_path, 'r') as merged_index, open(secondary_index_path, 'w') as secondary_index:
+        # last_token = None
+        j = 0
+
+        while True:
+            i = j
+
+            line = merged_index.readline()
+            # print(line)
+            # break
+            if not line:
+                break
+            
+            for _ in line:
+                j += 1
+
+            token, json_data = line.split(':', 1)  # split only once, so that the token can contain ":"
+
+            secondary_index.write(f'{token}:{i}\n')
 
 def calculate_tfidf(merged_index_path, total_documents):
-    temp_merged_index_path = merged_index_path + '.tmp'
-    
-    with open(merged_index_path, 'r') as merged_index_file, open(temp_merged_index_path, 'w') as temp_merged_index_file:
-        merged_index_reader = csv.DictReader(merged_index_file)
-        fieldnames = merged_index_reader.fieldnames
-        temp_merged_index_writer = csv.DictWriter(temp_merged_index_file, fieldnames=fieldnames)
-        temp_merged_index_writer.writeheader()
+    with open(merged_index_path, 'r') as merged_index, open(merged_index_path + '.tmp', 'w') as new_merged_index:
+        for line in merged_index:
+            token, data_str = line.strip().split(':', 1)
+            try:
+                data = json.loads(data_str)
 
-        for row in merged_index_reader:
-            token_data = json.loads(row['Data'])
+                idf = math.log(total_documents / data['document_freq'])
 
-            idf = math.log(total_documents / token_data['document_freq'])
-            for doc_id, doc_info in token_data['doc_ids'].items():
-                tf = doc_info['freq']  # changed to get the term frequency for this specific document
-                tf_idf = (1 + math.log(tf)) * idf
-                doc_info['tf_idf'] = tf_idf
+                for doc_id, doc_info in data['doc_ids'].items():
+                    tf = 1 + math.log(doc_info['freq'])
+                    tf_idf = tf * idf
 
-            temp_merged_index_writer.writerow({'Token': row['Token'], 'Data': json.dumps(token_data)})
+                    doc_info['tf_idf'] = tf_idf * doc_info['weight']
+
+                new_merged_index.write(f'{token}:{json.dumps(data)}\n')
+
+            except json.JSONDecodeError:
+                print(f"Error parsing JSON data for line: {line}")
+                continue
 
     os.remove(merged_index_path)
-    os.rename(temp_merged_index_path, merged_index_path) 
-
-
+    os.rename(merged_index_path + '.tmp', merged_index_path)
--- partial_indexer.py
+++ partial_indexer.py
@@ -1,7 +1,7 @@
-import csv
 import re
 import sys
 import json
+import math
 from collections import defaultdict
 from bs4 import BeautifulSoup
 from nltk.stem import SnowballStemmer
@@ -9,7 +9,7 @@ from urllib.parse import urldefrag
 
 class PartialIndexer:
     def __init__(self):
-        # Stemmer from nltk library - test with PorterStemmer too
+        # Stemmer from nltk library
         self.stemmer = SnowballStemmer("english")
 
         # Structure of index
@@ -36,151 +36,90 @@ class PartialIndexer:
         #   }
         # }
 
+        # Weights assigned to HTML tags
         self.HTML_WEIGHTS = {
-            # Title added additional weight because of extreme importance
-            'title': 25, 
-
-            # Headers
-            'h1': 10,
-            'h2': 9,
-            'h3': 7,
-            'h4': 4,
-            'h5': 2,
-            'h6': 1,
-
-            # Bold
-            'b': 3,
-            'strong': 3,
-
-            # Italic
-            'i': 2,
-            'em': 2
+            'title': 20,  # Title has highest weight
+            'h1': 10, 'h2': 9, 'h3': 7, 'h4': 4, 'h5': 2, 'h6': 1,  # Headers
+            'b': 3, 'strong': 3,  # Bold
+            'i': 2, 'em': 2  # Italic
         }
 
         # Current size of index
         self.current_size = 0
 
         # Max size before we create a partial index
-        self.max_size = 5242880 # 5mb
+        self.max_size = 200000
 
         # Unique doc id we increment (hash value)
         self.id_counter = 1
 
         # URL to unique ID map
-        # Useful to first check if url already has an assigned id without having to check file
         self.url_id_map = {}
 
     def tokenize_and_stem(self, text):
-        # Use regex expression
         tokens = re.findall(r'\b[a-zA-Z0-9]+\b', text)
-
-        # Lower and stem before returning list of tokens
         return [self.stemmer.stem(token.lower()) for token in tokens]
 
     def add_document(self, document, document_url, url_id_map_path):
-        # Defrag the url first
         document_url, _ = urldefrag(document_url)
-
-        # Check if the URL already exists in the map
         if document_url in self.url_id_map:
             return
 
-        # Generate ID for the document
         id = self.get_id(document_url)
-
-        # Parse the document
         soup = BeautifulSoup(document, 'html.parser')
 
-        # Add the document URL and its ID to the URL-ID csv file
         self.add_url_to_map(document_url, id, url_id_map_path)
 
-        # Add the document URL and its ID to the URL-ID csv file
-        self.add_url_to_map(document_url, id, url_id_map_path)
-
-        # Get all the text in the url and tokenize
         all_text = soup.get_text()
         tokens = self.tokenize_and_stem(all_text)
 
         for token in tokens:
-            # If this is the first occurrence of the token, initialize its entry in the index
-            if token not in self.index:
-                self.index[token] = {'token_freq': 0, 'document_freq': 0, 'doc_ids': defaultdict(lambda: {'id': id, 'freq': 0, 'weight': 0, 'tf_idf': 0})}
-
-            # Increase the total frequency of the token
             self.index[token]['token_freq'] += 1
-
-            # If this is the first occurrence of the token in this document, initialize its entry in doc_ids and increase the document frequency
             if id not in self.index[token]['doc_ids']:
                 self.index[token]['document_freq'] += 1
                 self.index[token]['doc_ids'][id] = {'id': id, 'freq': 1, 'weight': 0, 'tf_idf': 0}
             else:
-                # Otherwise, just increase the frequency of the token in this document
                 self.index[token]['doc_ids'][id]['freq'] += 1
 
-        # Process each HTML tag in the document
-        for tag in soup.find_all():
-            # Get the weight of the tag
-            weight = self.HTML_WEIGHTS.get(tag.name, 1)
 
-            # Tokenize and stem the text within the tag
+        for tag in soup.find_all():
+            weight = self.HTML_WEIGHTS.get(tag.name, 1)
             important_tokens = self.tokenize_and_stem(tag.get_text())
 
             for token in important_tokens:
-                # If the token is in the index and its ID is in doc_ids, increase its weight
                 if token in self.index and id in self.index[token]['doc_ids']:
-                    self.index[token]['doc_ids'][id]['weight'] += weight
+                    tf = 1 + math.log(self.index[token]['doc_ids'][id]['freq'])
+                    if self.index[token]['document_freq'] != 0:
+                        idf = math.log(len(self.url_id_map) / self.index[token]['document_freq'])
+                    else:
+                        idf = 0
+                    tf_idf = tf * idf
+                    self.index[token]['doc_ids'][id]['weight'] += tf_idf * weight
+                    self.index[token]['doc_ids'][id]['tf_idf'] = tf_idf
 
-        # Update the current size of the index
         self.update_index_size()
-
-        # Increase the current size by the size of the document
         self.current_size += len(document.encode('utf-8'))
 
-
     def should_write_partial_index(self):
-        # If current size exceeds max size write a partial index
         return self.current_size >= self.max_size
 
     def update_index_size(self):
-        # Update index size
         self.current_size = sys.getsizeof(self.index)
 
     def write_partial_index(self, path):
-        # Write to a csv file
-        with open(path, 'w', newline='') as csvfile:
-            # Define the fields or columns in the CSV file
-            fieldnames = ['Token', 'Data']
-
-            # Create a CSV writer that writes dictionaries into the file and write header
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-
-            # Iterate over each token in the index
+        with open(path, 'w') as txtfile:
             for token in sorted(self.index.keys()):
-                # Write token index structure as shown above
-                writer.writerow({'Token': token, 'Data': json.dumps(self.index[token])})
+                txtfile.write(f'{token}: {json.dumps(self.index[token])}\n')
 
-        # Reset current size and clear index dictionary (might free up memory idk)
         self.current_size = 0
         self.index.clear()
 
     def get_id(self, url):
-        # Check if the URL is already in the url_id_map dictionary
         if url not in self.url_id_map:
-            # If not, assign the current id_counter value to this URL in the dictionary
             self.url_id_map[url] = self.id_counter
-
-            # Increment the id_counter for the next new URL's id value
             self.id_counter += 1
-        
-        # Return id
         return self.url_id_map[url]
 
     def add_url_to_map(self, url, id, filename):
-        # APPEND to csv file
-        with open(filename, mode='a', newline='', encoding='utf-8') as file:
-            # Create a CSV writer object that will write into the file
-            writer = csv.writer(file)
-
-            # Write a new row into the CSV file with the id and the url
-            writer.writerow([id, url])
+        with open(filename, mode='a', encoding='utf-8') as file:
+            file.write(f'{id}:{url}\n')
